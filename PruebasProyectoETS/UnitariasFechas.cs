e5919c1e62d6ffafc14089b4fb87050aeec93df7
Daliborka Vidic Gutiérrez <daliborka_vg@hotmail.com>
2022-03-29 11:33:06 +0100

    Prueba1

--- PruebasProyectoETS/UnitariasFechas.cs
+++ PruebasProyectoETS/UnitariasFechas.cs
@@ -1,27 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProyectoETS;
 using System.Globalization;
 using static ProyectoETS.Tratar_Fechas;
 using static ProyectoETS.Pedir_Check_Fecha;
+using System;
 
 namespace PruebasProyectoETS
 {
     [TestClass]
     public class UnitariasFechas
     {
-        [TestMethod]
-        public void TestAntesDeCriso()
+        /*[TestMethod]
+        public void TestAntesDeCristo()
         {
-            Pedir_Check_Fecha.
-        }
-
+            
+        }*/
         [TestMethod]
         public void TestDiffDia()
         {
-
+            DateTime fecha1 = new DateTime(17/01/1999);
+            DateTime fecha2 = new DateTime(21/03/1865);
+            int diff = 134;
+            Assert.AreEqual(diff, CalcularDiferenciaAnhos(fecha1, fecha2));
         }
 
-        [TestMethod]
+       /* [TestMethod]
         public void TestDiffAnhos()
         {
 
@@ -31,6 +33,6 @@ namespace PruebasProyectoETS
         public void TestDiffActual()
         {
 
-        }
+        }*/
     }
 }
